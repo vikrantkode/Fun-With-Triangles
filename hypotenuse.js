46fb3c9ddfb25a2b40515a0e477e059cddec506c
Vikrant <vikrant2812@gmail.com>
2021-10-11 10:54:55 +0530

    Updated Logic for negative inputs

--- hypotenuse.js
+++ hypotenuse.js
@@ -3,14 +3,22 @@ var hypotenuseBtn = document.querySelector("#hypotenuse-btn")
 var outputEl = document.querySelector("#output")
 
 
-function calculateSumOfSquares(a,b){
-var sumOfSquares = a*a + b*b;
-return sumOfSquares;
+function calculateSumOfSquares(a, b) {
+    var sumOfSquares = a * a + b * b;
+    return sumOfSquares;
 }
-function calculateHypotenuse(){
-var sumOfSquares = calculateSumOfSquares(Number(sides[0].value),Number(sides[1].value));
-var lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-outputEl.innerText = "The Length of hypotenuse is " + lengthOfHypotenuse;
+
+
+function calculateHypotenuse() {
+    if (Number(sides[0].value) === "" || (Number(sides[1].value) === "")) {
+        outputEl.innerText = "Enter Sides to calculate Hypotenuse...!";
+    } else if (Number(sides[0].value) < 0 || (Number(sides[1].value) < 0)) {
+        outputEl.innerText = "Please Enter Positive Inputs";
+    } else {
+        var sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+        var lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+        outputEl.innerText = "The Length of hypotenuse is " + lengthOfHypotenuse;
+    }
 }
 
 hypotenuseBtn.addEventListener("click", calculateHypotenuse)
